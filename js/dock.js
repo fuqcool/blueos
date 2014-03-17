4ff55dee1f34656df936f4a430f3d1c711ec947a
fuqcool <fuqcool@gmail.com>
2014-03-17 18:26:09 -0400

    show tooltip when mouse over app icon

--- js/dock.js
+++ js/dock.js
@@ -16,6 +16,14 @@ ferret.module('blueos.dock', function (exports, require, module) {
       });
 
       $icons.append(html);
+      $icons.tooltip({
+        position: {
+          my: 'right-7 center-32'
+        },
+        tooltipClass: 'app-tooltip',
+        show: false,
+        hide: false
+      });
     }
   });
 
